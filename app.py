39101a0fb5ad837ff71c9a69c222c17e5ea7b101
hulksmasher <somesh.08.96@gmail.com>
2019-09-12 22:55:53 +0530

    list, push, pop, remove implemented without error handling

--- app.py
+++ app.py
@@ -1,14 +1,55 @@
 from flask import Flask
-import json
+# import json, jsonify
+from flask import request, jsonify
+
 from models import LinkedListManager, LinkedList
 app = Flask(__name__)
 
 linked_list_manager = LinkedListManager()
 
-@app.route('/api/link', methods = ['GET'])
+@app.route('/api/link', methods = ['POST'])
 def create():
-    linked_list_manager.add()
-    return json.dumps(linked_list_manager.count)
+    data = request.form
+    if 'name' not in data.keys() or 'birthyear' not in data.keys():
+        return jsonify("Bad Parameters"), 400
+    linked_list = linked_list_manager.create_list(data['name'], data['birthyear'])
+    return jsonify(data = linked_list.to_json()), 200
+
+@app.route('/api/link/<id>', methods = ['GET'])
+def get_list(id):
+    linked_list = linked_list_manager.retrieve_list(id)
+    return jsonify(linked_list.to_json()), 200
+
+@app.route('/api/link/<id>', methods = ['PUT'])
+def push(id):
+    data = request.form
+    if 'name' not in data.keys() or 'birthyear' not in data.keys():
+        return jsonify("Bad Parameters"), 400
+    linked_list = linked_list_manager.retrieve_list(id)
+    linked_list.add_node(data['name'], data['birthyear'])
+    return jsonify(linked_list.to_json()), 200
+
+@app.route('/api/link/pop/<id>', methods = ['GET'])
+def pop(id):
+    linked_list = linked_list_manager.retrieve_list(id)
+    linked_list.pop_node()
+    return jsonify(linked_list.to_json()), 200
+
+@app.route('/api/link/remove/<id>', methods = ['POST'])
+def remove(id):
+    data = request.form
+    if 'name' not in data.keys() or 'birthyear' not in data.keys():
+        return jsonify("Bad Parameters"), 400
+    linked_list = linked_list_manager.retrieve_list(id)
+    linked_list.remove_node(data['name'], data['birthyear'])
+    return jsonify(linked_list.to_json()), 200
+
+@app.route('/api/link/pop/<id>', methods = ['GET'])
+def reverse(id):
+    linked_list = linked_list_manager.retrieve_list(id)
+    linked_list.pop_node()
+    return jsonify(linked_list.to_json()), 200
+
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -2,13 +2,92 @@ class LinkedListManager(object):
     linked_lists = {}
     count = 0
 
-    def add(self):
-        self.linked_lists[100 + self.count] = LinkedList(100 + self.count)
+    def create_list(self, name, birthyear):
+        new_id = str(100 + self.count + 1)
+        linked_list = LinkedList(name, birthyear)
+        linked_list._id = new_id
+        self.linked_lists[new_id] = linked_list
         self.count = self.count + 1
+        return linked_list
+
+    def retrieve_list(self, id):
+        return self.linked_lists[id]
 
     def delete(self, id):
         del self.linked_lists[id]
 
 class LinkedList(object):
-    def __init__(self, id):
-        self.id = id
+    length = 0
+    _id = 0
+    new_node_id = 1
+    head = None
+    curr = None
+    def __init__(self, name, birthyear):
+        self.add_node(name, birthyear)
+        if self.head is None: self.head = self.curr
+    
+    def add_node(self, name, birthyear):
+        new_id = self.new_node_id
+        new_node = Node(new_id, name, birthyear)
+        if self.curr is not None:
+            self.curr.next = new_node
+            new_node.prev = self.curr
+        self.curr = new_node
+        self.length = self.length + 1
+        self.new_node_id = self.new_node_id + 1
+
+    def to_json(self):
+        all_nodes = self.get_nodes()
+        data = {'meta': {'id': self._id, 'length': self.length},
+            'object': [node.to_json() for node in all_nodes]
+        }
+        return data
+    
+    def pop_node(self):
+        if self.curr == self.head: self.head = None
+        self.curr = self.curr.prev
+        self.curr.next = None
+        self.length = self.length - 1
+
+    def remove_node(self, name, birthyear):
+        i = self.head
+        while i is not None:
+            if i.name == name and i.birthyear == birthyear:
+                if i == self.head: self.head = self.head.next
+                if i.prev is not None: i.prev.next = i.next
+                if i.next is not None: i.next.prev = i.prev
+                self.length = self.length - 1
+                return i
+            i = i.next
+        return None
+        
+
+    def get_nodes(self):
+        i = self.head
+        nodes = []
+        while(i is not None):
+            nodes.append(i)
+            i = i.next
+            print(i)
+        return nodes
+    
+class Node(object):
+    _id = 0
+    next = None
+    prev = None
+    def __init__(self, new_id, name, birthyear):
+        self._id = new_id
+        self.name = name
+        self.birthyear = birthyear
+    
+    def to_json(self):
+        data = {
+            'id': self._id,
+            'name': self.name,
+            'birthyear': self.birthyear,
+            'next': str(self.next)
+        }
+        return data
+    
+    def __str__(self):
+        return str(self._id)
